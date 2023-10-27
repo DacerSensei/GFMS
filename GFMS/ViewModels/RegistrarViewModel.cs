b6a6b6f86457e0872732916ab3d0b82e166e4684
Dave Orijuela <daveorijuela@yahoo.com>
2023-10-27 23:12:52 +0800

    Finished writing the commands and view of registrar

--- GFMS/ViewModels/RegistrarViewModel.cs
+++ GFMS/ViewModels/RegistrarViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using GFMS.Commands;
 using GFMS.Core;
+using GFMS.ViewModels.RegistrarViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,9 +14,67 @@ namespace GFMS.ViewModels
     {
         public RegistrarViewModel()
         {
+            CurrentView = MainPageView;
 
+            DashboardCommand = new Command(action =>
+            {
+                CurrentView = DashboardView;
+            });
+            OfficiallyEnrolledCommand = new Command(action =>
+            {
+                CurrentView = OfficiallyEnrolledView;
+            });
+            RegisterStudentCommand = new Command(action =>
+            {
+                CurrentView = RegisterStudentView;
+            });
+            ReportCardCommand = new Command(action =>
+            {
+                CurrentView = ReportCardView;
+            });
+            RequirementsCommand = new Command(action =>
+            {
+                CurrentView = RequirementsView;
+            });
+            StudentRecordsCommand = new Command(action =>
+            {
+                CurrentView = StudentRecordsView;
+            });
+            MainPageCommand = new Command(action =>
+            {
+                CurrentView = MainPageView;
+            });
         }
 
+        public ICommand DashboardCommand { get; }
+        public ICommand OfficiallyEnrolledCommand { get; }
+        public ICommand RegisterStudentCommand { get; }
+        public ICommand ReportCardCommand { get; }
+        public ICommand RequirementsCommand { get; }
+        public ICommand StudentRecordsCommand { get; }
+        public ICommand MainPageCommand { get; }
+
+        private RegistrarDashboardViewModel DashboardView = new();
+        private RegistrarOfficiallyEnrolledViewModel OfficiallyEnrolledView = new();
+        private RegistrarRegisterStudentViewModel RegisterStudentView = new();
+        private RegistrarReportCardViewModel ReportCardView = new();
+        private RegistrarRequirementsViewModel RequirementsView = new();
+        private RegistrarStudentRecordsViewModel StudentRecordsView = new();
+        private MainPageViewModel MainPageView = new();
+
         public ICommand LogoutCommand { get; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged(nameof(CurrentView));
+            }
+        }
+
     }
 }
