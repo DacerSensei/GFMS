2e8a6a7c38a58d5ebba7c1a0c04c939684352f89
Dave Orijuela <daveorijuela@yahoo.com>
2023-11-07 10:24:16 +0800

    Initial Models and View in Principal

--- GFMS/ViewModels/PrincipalViewModel.cs
+++ GFMS/ViewModels/PrincipalViewModel.cs
@@ -1,13 +1,75 @@
-﻿using GFMS.Core;
+﻿using GFMS.Commands;
+using GFMS.Core;
+using GFMS.ViewModels.FinanceViewModels;
+using GFMS.ViewModels.PrincipalViewModels;
+using GFMS.Views;
+using GFMS.Views.Modals;
+using MaterialDesignThemes.Wpf;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace GFMS.ViewModels
 {
     public class PrincipalViewModel : ViewModelBase
     {
+        public PrincipalViewModel()
+        {
+            CurrentView = MainPageView;
+
+            RegistrarCommand = new Command(action =>
+            {
+                CurrentView = RegistrarView;
+            });
+            FinanceCommand = new Command(action =>
+            {
+                CurrentView = FinanceView;
+            });
+            TeacherCommand = new Command(action =>
+            {
+                CurrentView = TeacherView;
+            });
+
+            LogoutCommand = new Command(async action =>
+            {
+                var result = await DialogHost.Show(new AlertDialog("Notice", "Are you sure you want to logout?"), "RootDialog");
+                if ((bool)result! != true)
+                {
+                    return;
+                }
+                Login login = new Login();
+                login.Show();
+                Application.Current.MainWindow.Close();
+                Application.Current.MainWindow = login;
+
+            });
+        }
+
+        public ICommand RegistrarCommand { get; }
+        public ICommand FinanceCommand { get; }
+        public ICommand TeacherCommand { get; }
+
+        private readonly MainPageViewModel MainPageView = new();
+        private readonly PrincipalRegistrarViewModel RegistrarView = new();
+        private readonly PrincipalFinanceViewModel FinanceView = new();
+        private readonly PrincipalTeacherViewModel TeacherView = new();
+
+        public ICommand LogoutCommand { get; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged(nameof(CurrentView));
+            }
+        }
     }
 }
